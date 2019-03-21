a6a1a1dd814491ecbeff8505b7ea72dfbfae6744
Christopher McIntosh <christopher.l.mcintosh@outlook.com>
2019-03-21 23:08:28 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -10,8 +10,6 @@ def second_challenge
   just_values << groceries[vegetable].values
   just_values << groceries[meat].values
   just_values << groceries[grains].values
-  just_values.
-
-  
-
+  just_values.flatten
+  just_values
 end
